6395e4de0a9c1f930383ae61e6cbcbb20cdc5f3c
Ian Burns <burnsie@vcu.edu>
2019-08-02 21:04:27 -0400

    Auth Routing Changes

--- authroutes.js
+++ authroutes.js
@@ -1,6 +1,8 @@
 const path = require('path');
 const passport = require('passport');
 
+const defaultRedirect = '/me';
+
 module.exports = function(app) {
   // api routes
   app.get('/api/user', function (req, res) {
@@ -13,24 +15,24 @@ module.exports = function(app) {
     });
   });
 
-  app.use('/api/register', passport.authenticate('local-register', { successRedirect : '/medirect', failureRedirect : '/register', failureFlash: true, successFlash: 'Welcome!' }));
-  app.post('/api/login', passport.authenticate('local-login', { successRedirect : '/medirect', failureRedirect : '/login', failureFlash: true, successFlash: 'Welcome!' }));
+  app.use('/api/register', passport.authenticate('local-register', { successRedirect : defaultRedirect, failureRedirect : '/register', failureFlash: true, successFlash: 'Welcome!' }));
+  app.post('/api/login', passport.authenticate('local-login', { successRedirect : defaultRedirect, failureRedirect : '/login', failureFlash: true, successFlash: 'Welcome!' }));
 
-  app.use('/medirect', isLoggedIn, function(req, res) {
+  app.use('/me', isLoggedIn, function(req, res) {
     res.render('pages/me', { data: { messages: req.flash() }});
   })
 
   // login route
-  app.get('/login', function(req, res) {
+  app.get('/login', isNotLoggedIn, function(req, res) {
     res.render('pages/login', { data: { messages: req.flash() } });
   });
 
-  // login route
-  app.get('/register', function(req, res) {
+  // register route
+  app.get('/register', isNotLoggedIn, function(req, res) {
     res.render('pages/register', { data: { messages: req.flash() } });
   });
 
-  // login route
+  // logout route
   app.use('/logout', function(req, res) {
     console.log("LOGOUT: \"" + req.user.Username + "\" (Id: " + req.user.Id + ") has logged out.")
     req.logout();
@@ -42,6 +44,13 @@ module.exports = function(app) {
 function isLoggedIn(req, res, next) {
   if (req.isAuthenticated())
       return next();
-  res.redirect('/login');//path.join(__dirname, 'public') + '/login.html');
+  res.redirect('/login');
+}
+
+//route middleware to ensure user is not logged in
+function isNotLoggedIn(req, res, next) {
+  if (!req.isAuthenticated())
+      return next();
+  res.redirect(defaultRedirect);
 }
 
